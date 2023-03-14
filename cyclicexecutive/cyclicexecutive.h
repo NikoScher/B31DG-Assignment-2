e850c4bff291651ce59a1a37c60ba9f0e1eb5ebf
Niko <niko.scher.26@gmail.com>
2023-03-14 21:55:44 +0000

    Implemented tasks 1 to 5 and found worst time for each

---
+++ cyclicexecutive/cyclicexecutive.h
@@ -0,0 +1,51 @@
+/*
+*   cyclicexecutive.h
+*   
+*   Header file for assignment 2 for ESP32C3 Dev Module
+*   Nikolaus Scherwitzel (H00298068)
+*   Created: 13/03/23
+*/
+
+#ifndef __CYCLICEXECUTIVE_H__
+#define __CYCLICEXECUTIVE_H__
+
+// Pin numbers
+#define T1_PIN        1   // blah
+#define T2_PIN        2   // blah
+#define T3_PIN        3   // blah
+#define T4_ANIN_PIN   4   // blah
+#define T4_LED_PIN    0   // blah
+
+// Schedule
+#define TASK1_P       1   // blah
+#define TASK2_P       1   // blah
+#define TASK3_P       1   // blah
+#define TASK4_P       1   // blah
+#define TASK5_P       1   // blah
+
+// Unique parameters
+#define BAUD_RATE       9600  // baud reate
+#define TICK_LENGTH     4     // Period
+
+#define NUM_PARAMS      4     // thingy
+#define MAX_VOLTAGE     3.3   // bah
+
+#define TASK2_TIMEOUT   3003  // bah
+#define TASK2_MINFREQ   333   // blah
+#define TASK2_MAXFREQ   1000  // blah
+
+#define TASK3_TIMEOUT   2000  // bah
+#define TASK3_MINFREQ   500   // blah
+#define TASK3_MAXFREQ   1000  // blah
+
+#define TASK4_THRESH    2048  // blah
+
+#define TASK5_MIN       0     // blah
+#define TASK5_MAX       99    // blah
+
+// Helpful typedefs
+typedef unsigned char uint8;
+typedef unsigned int uint16;
+typedef unsigned long uint32;
+
+#endif  // __CYCLICEXECUTIVE_H__
---
+++ cyclicexecutive/cyclicexecutive.ino
@@ -0,0 +1,135 @@
+
+/*
+*   cyclicexecutive.ino
+*   
+*   Sketch for assignment 2 for ESP32C3 Dev Module
+*   Nikolaus Scherwitzel (H00298068)
+*   Created: 13/03/23
+*/
+
+#include "cyclicexecutive.h"
+#include <B31DGMonitor.h>
+#include <Ticker.h>
+
+#define periodToFreq_us(T) (1 / (T / 1000000))
+
+// Globals
+B31DGCyclicExecutiveMonitor monitor;
+
+uint8 tCount;
+Ticker ticker;
+
+double freqT2;
+double freqT3;
+
+uint16 anIn[NUM_PARAMS];
+uint8 currInd;
+
+void setup() {
+  Serial.begin(BAUD_RATE);
+
+  pinMode(T1_PIN, OUTPUT); 
+  pinMode(T2_PIN, INPUT);
+  pinMode(T3_PIN, INPUT); 
+  pinMode(T4_ANIN_PIN, INPUT); 
+  pinMode(T4_LED_PIN, OUTPUT); 
+
+  currInd = 0;
+  for (uint8 i = 0; i < NUM_PARAMS; i++) {
+    anIn[i] = 0;
+  }
+  
+  tCount = 0;
+  ticker.attach_ms(TICK_LENGTH, cycle);
+  
+  //monitor.startMonitoring();
+}
+
+void cycle() {
+  if ((tCount % TASK1_P) == 0)  task1();
+  if ((tCount % TASK2_P) == 0)  task2();
+  if ((tCount % TASK3_P) == 0)  task3();
+  if ((tCount % TASK4_P) == 0)  task4();
+  if ((tCount % TASK5_P) == 0)  task5();
+  
+  tCount++;
+}
+
+void task1() {
+  //monitor.jobStarted(1);
+  
+  digitalWrite(T1_PIN, HIGH);
+  delayMicroseconds(200);
+  digitalWrite(T1_PIN, LOW);
+  delayMicroseconds(50);
+  digitalWrite(T1_PIN, HIGH);
+  delayMicroseconds(30);
+  digitalWrite(T1_PIN, LOW);
+
+  //monitor.jobEnded(1);
+}
+
+void task2() {
+  //monitor.jobStarted(2);
+  
+  double period = (double) pulseIn(T2_PIN, HIGH) * 2;
+  freqT2 = periodToFreq_us(period);
+
+  //monitor.jobEnded(2);
+}
+
+void task3() {
+  //monitor.jobStarted(3);
+  
+  double period = (double) pulseIn(T3_PIN, HIGH) * 2;
+  freqT3 = periodToFreq_us(period);
+
+  //monitor.jobEnded(3);
+}
+
+void task4() {
+  //monitor.jobStarted(4);
+  
+  anIn[currInd] = analogRead(T4_ANIN_PIN);
+  currInd = (currInd + 1) % NUM_PARAMS;
+  
+  double filtAnIn = 0;
+  for (uint8 i = 0; i < NUM_PARAMS; i++) {
+    filtAnIn += anIn[i];
+  }
+  filtAnIn /= NUM_PARAMS;
+
+  digitalWrite(T4_LED_PIN, (filtAnIn > TASK4_THRESH));
+
+  //monitor.jobEnded(4);
+}
+
+void task5() {
+  //monitor.jobStarted(5);
+  
+  uint8 normFreqT2 = map(freqT2, TASK2_MINFREQ, TASK2_MAXFREQ, TASK5_MIN, TASK5_MAX);
+  normFreqT2 = constrain(normFreqT2, TASK5_MIN, TASK5_MAX);
+  uint8 normFreqT3 = map(freqT3, TASK3_MINFREQ, TASK3_MAXFREQ, TASK5_MIN, TASK5_MAX);
+  normFreqT3 = constrain(normFreqT3, TASK5_MIN, TASK5_MAX);
+  Serial.print(normFreqT2);
+  Serial.print(",");
+  Serial.println(normFreqT3);
+
+  //monitor.jobEnded(5);
+}
+
+void loop() {
+  int ITERS = 100;
+  long int avg = 0;
+  for (int i = 0; i < ITERS; i++) {
+    long int start = micros();
+    task5();
+    avg += micros() - start;
+    delay(30);
+  }
+  avg /= ITERS;
+  Serial.println();
+  Serial.print(avg);
+  Serial.print("us");
+  Serial.println();
+}
