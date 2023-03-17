45d722f44635539f228cc40f86b952e49dd9c43b
Niko <niko.scher.26@gmail.com>
2023-03-17 16:22:41 +0000

    Additional comments

--- cyclicexecutive/cyclicexecutive.h
+++ cyclicexecutive/cyclicexecutive.h
@@ -10,38 +10,38 @@
 #define __CYCLICEXECUTIVE_H__
 
 // Pin numbers
-#define T1_PIN        1   // blah
-#define T2_PIN        2   // blah
-#define T3_PIN        3   // blah
-#define T4_ANIN_PIN   4   // blah
-#define T4_LED_PIN    0   // blah
+#define T1_PIN        1   // Task 1 output signal pin
+#define T2_PIN        2   // Task 2 input measure signal pin
+#define T3_PIN        3   // Task 3 input measure signal pin
+#define T4_ANIN_PIN   4   // Task 4 analogue input pin
+#define T4_LED_PIN    0   // Task 4 LED output pin
 
 // Schedule
-#define TASK2_P       10  // blah
-#define TASK2_POFF1   1   // blah
-#define TASK2_POFF2   7   // blah
-#define TASK3_P       2   // blah
-#define TASK4_P       5   // blah
-#define TASK5_P       25  // blah
+#define TASK2_P       10  // Task 2 period: repeats every 10 frames
+#define TASK2_POFF1   1   // Task 2 offset from frame 0
+#define TASK2_POFF2   7   // Another task 2 offset from frame 0
+#define TASK3_P       2   // Task 3 period: repeats every 2 frames
+#define TASK4_P       5   // Task 4 period: repeats every 5 frames
+#define TASK5_P       25  // Task 5 period: repeats every 25 frames
 
-// Unique parameters
-#define BAUD_RATE       9600  // baud reate
-#define TICK_LENGTH     4     // Period
-#define NUM_FRAMES      50    // blah
+// Task parameters
+#define BAUD_RATE       9600  // Baud rate
+#define TICK_LENGTH     4     // Length of time per tick (frame) in ms
+#define NUM_FRAMES      50    // Number of frames before schedule is repeated
 
-#define TASK2_TIMEOUT   3100  // bah
-#define TASK2_MINFREQ   333   // blah
-#define TASK2_MAXFREQ   1000  // blah
+#define TASK2_TIMEOUT   3100  // Timeout for pulseIn = worst case in us
+#define TASK2_MINFREQ   333   // Task 2 minimium frequency of waveform in Hz
+#define TASK2_MAXFREQ   1000  // Task 2 maximium frequency of waveform in Hz
 
-#define TASK3_TIMEOUT   2100  // bah
-#define TASK3_MINFREQ   500   // blah
-#define TASK3_MAXFREQ   1000  // blah
+#define TASK3_TIMEOUT   2100  // Timeout for pulseIn = worst case in us
+#define TASK3_MINFREQ   500   // Task 3 minimium frequency of waveform in Hz
+#define TASK3_MAXFREQ   1000  // Task 3 maximium frequency of waveform in Hz
 
-#define NUM_PARAMS      4     // thingy
-#define TASK4_THRESH    2048  // blah
+#define NUM_PARAMS      4     // Task 4 length of array for storing past measurements
+#define TASK4_THRESH    2048  // Task 4 threshold for turning LED on
 
-#define TASK5_MIN       0     // blah
-#define TASK5_MAX       99    // blah
+#define TASK5_MIN       0     // Task 5 lower bound of range
+#define TASK5_MAX       99    // Task 5 upper bound of range
 
 // Helpful typedefs
 typedef unsigned char uint8;
--- cyclicexecutive/cyclicexecutive.ino
+++ cyclicexecutive/cyclicexecutive.ino
@@ -7,59 +7,71 @@
 *   Created: 13/03/23
 */
 
+// NOTE: frame == tick, used interchangeably in comments
+
+// Libraries
 #include "cyclicexecutive.h"
 #include <B31DGMonitor.h>
 #include <Ticker.h>
 
+// Convert period in us to frequency in Hz
 #define periodToFreq_us(T) (1 / (T / 1000000))
 
 // Globals
 B31DGCyclicExecutiveMonitor monitor;
 
-uint8 tCount;
-Ticker ticker;
+uint8 tCount;   // Tick counter, is reset after 50 frames
+Ticker ticker;  // Ticker object
 
-double freqT2;
-double freqT3;
+double freqT2;  // Calculated frequency for task 2 waveform
+double freqT3;  // Calculated frequency for task 3 waveform
 
-uint16 anIn[NUM_PARAMS];
-uint8 currInd;
+uint16 anIn[NUM_PARAMS];  // Array for storing previous analogue measurements
+uint8 currInd;            // Current index to overwrite in anIn[]
 
 void setup() {
   Serial.begin(BAUD_RATE);
 
+  // Define pin inputs/outputs
   pinMode(T1_PIN, OUTPUT); 
   pinMode(T2_PIN, INPUT);
   pinMode(T3_PIN, INPUT); 
   pinMode(T4_ANIN_PIN, INPUT); 
   pinMode(T4_LED_PIN, OUTPUT); 
 
+  // Initialise task 4 analogue input array with 0's
   currInd = 0;
   for (uint8 i = 0; i < NUM_PARAMS; i++) {
     anIn[i] = 0;
   }
 
+  // Start monitoring and ticking
   tCount = 0;
   monitor.startMonitoring();
   ticker.attach_ms(TICK_LENGTH, cycle);
-  cycle();
+  cycle();  // Ran as first call of cycle from ticker is after TICK_LENGTH ms
 }
 
 void cycle() {
+  // Schedule, run tasks periodically according to TASKX_P in frames
   task1();
+  // Task 2 doesn't start execution at frame 0, so offset needed
   if (((tCount - TASK2_POFF1) % TASK2_P) == 0) task2();
   if (((tCount - TASK2_POFF2) % TASK2_P) == 0) task2();
   if ((tCount % TASK3_P) == 0)  task3();
   if ((tCount % TASK4_P) == 0)  task4();
   if ((tCount % TASK5_P) == 0)  task5();
   
+  // Increment tick and reset if hyper period reached (frame 50)
   tCount++;
   if (tCount > NUM_FRAMES)  tCount = 1;
 }
 
+// Period = 4ms / Rate = 250Hz
 void task1() {
   monitor.jobStarted(1);
   
+  // Generate waveform
   digitalWrite(T1_PIN, HIGH);
   delayMicroseconds(200);
   digitalWrite(T1_PIN, LOW);
@@ -71,48 +83,64 @@ void task1() {
   monitor.jobEnded(1);
 }
 
+// Period = 20ms / Rate = 50Hz
 void task2() {
   monitor.jobStarted(2);
-  
+
+  // Measure period when signal is HIGH
+  // Multiply by 2 because 50% duty cycle
   double period = (double) pulseIn(T2_PIN, HIGH) * 2;
-  freqT2 = periodToFreq_us(period);
+  freqT2 = periodToFreq_us(period); // Convert to frequency using T = 1/f
 
   monitor.jobEnded(2);
 }
 
+// Period = 8ms / Rate = 125Hz
 void task3() {
   monitor.jobStarted(3);
   
+  // Measure period when signal is HIGH
+  // Multiply by 2 because 50% duty cycle
   double period = (double) pulseIn(T3_PIN, HIGH) * 2;
-  freqT3 = periodToFreq_us(period);
+  freqT3 = periodToFreq_us(period); // Convert to frequency using T = 1/f
 
   monitor.jobEnded(3);
 }
 
+// Period = 20ms / Rate = 50Hz
 void task4() {
   monitor.jobStarted(4);
   
+  // Read analogue signal and increment index for next reading
+  // Analogue signal converted to 12 bit integer
   anIn[currInd] = analogRead(T4_ANIN_PIN);
   currInd = (currInd + 1) % NUM_PARAMS;
   
+  // Sum array and divide to get average
   double filtAnIn = 0;
   for (uint8 i = 0; i < NUM_PARAMS; i++) {
     filtAnIn += anIn[i];
   }
   filtAnIn /= NUM_PARAMS;
 
+  // Turn on LED if average is above half maximium of 12 bit integer (4096 / 2 = THRESH)
   digitalWrite(T4_LED_PIN, (filtAnIn > TASK4_THRESH));
 
   monitor.jobEnded(4);
 }
 
+// Period = 100ms / Rate = 10Hz
 void task5() {
   monitor.jobStarted(5);
   
+  // Map frequencies from 333Hz & 500Hz - 1000Hz to 0 - 99
+  // Constrain to 0 - 99 as map() only creates gradient
   int normFreqT2 = map(freqT2, TASK2_MINFREQ, TASK2_MAXFREQ, TASK5_MIN, TASK5_MAX);
   normFreqT2 = constrain(normFreqT2, TASK5_MIN, TASK5_MAX);
   int normFreqT3 = map(freqT3, TASK3_MINFREQ, TASK3_MAXFREQ, TASK5_MIN, TASK5_MAX);
   normFreqT3 = constrain(normFreqT3, TASK5_MIN, TASK5_MAX);
+
+  // Print to serial monitor
   Serial.print(normFreqT2);
   Serial.print(",");
   Serial.println(normFreqT3);
@@ -123,16 +151,14 @@ void task5() {
 void loop() {
   // Debug Timing
   /*
-  int ITERS = 100;
-  long int avg = 0;
-  for (int i = 0; i < ITERS; i++) {
-    long int start = micros();
-    task5();
+  uint16 ITERS = 100;
+  uint32 avg = 0;
+  for (uint16 i = 0; i < ITERS; i++) {
+    uint32 start = micros();
+    task1();
     avg += micros() - start;
-    delay(30);
   }
   avg /= ITERS;
-  Serial.println();
   Serial.print(avg);
   Serial.println("us");
   */
